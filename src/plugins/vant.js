d5d2ec96fba8b8af9ca6eb71f2f87edfb66200f8
冬夜 <zk_19881227@163.com>
2019-11-18 12:18:11 +0800

    完成功能home主页功能

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, NavBar, Tabbar, TabbarItem, Grid, GridItem } from 'vant'
+import { Button, NavBar, Tabbar, TabbarItem, Grid, GridItem, Swipe, SwipeItem, Lazyload, Image } from 'vant'
 
 Vue.use(Button)
 Vue.use(NavBar)
@@ -7,3 +7,9 @@ Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(Grid)
 Vue.use(GridItem)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Lazyload, {
+  lazyComponent: true
+})
+Vue.use(Image)
