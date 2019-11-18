0ae490f58f0710179227d1eb85a38cd9f456caae
冬夜 <zk_19881227@163.com>
2019-11-18 20:46:44 +0800

    解决商品列页冲突

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,22 +1,5 @@
 import Vue from 'vue'
-<<<<<<< HEAD
-import { Button, NavBar, Tabbar, TabbarItem, Grid, GridItem, Swipe, SwipeItem, Lazyload, Image, Stepper, Toast, Icon } from 'vant'
-=======
-import {
-  Button,
-  NavBar,
-  Tabbar,
-  TabbarItem,
-  Grid,
-  GridItem,
-  Card,
-  Image,
-  List,
-  PullRefresh,
-  Toast,
-  Cell
-} from 'vant'
->>>>>>> 5360e1a3df2284d7de2b7d63d868feb2f96b541d
+import { Button, NavBar, Tabbar, TabbarItem, Grid, GridItem, Card, Swipe, SwipeItem, Lazyload, Image, Stepper, Toast, Icon, List, PullRefresh, Cell } from 'vant'
 
 Vue.use(Button)
 Vue.use(NavBar)
@@ -24,7 +7,6 @@ Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(Grid)
 Vue.use(GridItem)
-<<<<<<< HEAD
 Vue.use(Swipe)
 Vue.use(SwipeItem)
 Vue.use(Lazyload)
@@ -32,11 +14,9 @@ Vue.use(Image)
 Vue.use(Stepper)
 Vue.use(Toast)
 Vue.use(Icon)
-=======
 Vue.use(Card)
 Vue.use(Image)
 Vue.use(List)
 Vue.use(PullRefresh)
 Vue.use(Toast)
 Vue.use(Cell)
->>>>>>> 5360e1a3df2284d7de2b7d63d868feb2f96b541d
