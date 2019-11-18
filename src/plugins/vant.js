98ea3a63f7c2c3149b8dfaed7d530f5aa412fdf8
Yusee <5376316+yusee@user.noreply.gitee.com>
2019-11-18 10:42:41 +0800

    完成了进入图片分享后的图片列表展示切换功能

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, NavBar, Tabbar, TabbarItem, Grid, GridItem } from 'vant'
+import { Button, NavBar, Tabbar, TabbarItem, Grid, GridItem, Tabs, Tab, Lazyload } from 'vant'
 
 Vue.use(Button)
 Vue.use(NavBar)
@@ -7,3 +7,6 @@ Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(Grid)
 Vue.use(GridItem)
+Vue.use(Tabs)
+Vue.use(Tab)
+Vue.use(Lazyload)
