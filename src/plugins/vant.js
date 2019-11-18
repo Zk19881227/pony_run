457ce8dc652e0680af36b2085036cb4241720d26
mr.fu <xiaoming@163.com>
2019-11-18 15:28:04 +0800

    符义浩第一次提交新闻详情页面

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, NavBar, Tabbar, TabbarItem, Grid, GridItem } from 'vant'
+import { Button, NavBar, Tabbar, TabbarItem, Grid, GridItem, Notify, Toast, Field } from 'vant'
 
 Vue.use(Button)
 Vue.use(NavBar)
@@ -7,3 +7,6 @@ Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(Grid)
 Vue.use(GridItem)
+Vue.use(Notify)
+Vue.use(Toast)
+Vue.use(Field)
