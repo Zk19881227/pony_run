4f62c12ead151796be8fedc4d45d98d2c1ad1970
夜霖丶辉 <llw91785@163.com>
2019-11-18 14:54:06 +0800

    购物车模板

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,5 +1,20 @@
 import Vue from 'vue'
-import { Button, NavBar, Tabbar, TabbarItem, Grid, GridItem } from 'vant'
+import {
+  Button,
+  NavBar,
+  Tabbar,
+  TabbarItem,
+  Grid,
+  GridItem,
+  SubmitBar,
+  Stepper,
+  Card,
+  Tag,
+  PullRefresh,
+  SwipeCell,
+  Toast,
+  Row
+} from 'vant'
 
 Vue.use(Button)
 Vue.use(NavBar)
@@ -7,3 +22,11 @@ Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(Grid)
 Vue.use(GridItem)
+Vue.use(SubmitBar)
+Vue.use(Stepper)
+Vue.use(Card)
+Vue.use(Tag)
+Vue.use(PullRefresh)
+Vue.use(SwipeCell)
+Vue.use(Toast)
+Vue.use(Row)
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    goodsList: [],
+    totalNum: 0
   },
   mutations: {
+    joinCar(state, params) {
+      const index = state.goodsList.findIndex(item => item.id === params.id)
+      state.totalNum += params.num
+      if (index !== -1) {
+        state.goodsList[index].num += params.num
+        return
+      }
+      state.goodsList.push(params)
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  getters: {},
+  modules: {}
 })
