676d9032a0810e0e362c0c24c4d2f8c8262c2a7e
wumai <wumai@itcast.cn>
2019-11-18 21:15:47 +0800

    黄健

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, NavBar, Tabbar, TabbarItem, Grid, GridItem } from 'vant'
+import { Button, NavBar, Tabbar, TabbarItem, Grid, GridItem, ImagePreview, Lazyload, Field, Cell, CellGroup, Toast } from 'vant'
 
 Vue.use(Button)
 Vue.use(NavBar)
@@ -7,3 +7,9 @@ Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(Grid)
 Vue.use(GridItem)
+Vue.use(ImagePreview)
+Vue.use(Lazyload)
+Vue.use(Field)
+Vue.use(Cell)
+Vue.use(CellGroup)
+Vue.use(Toast)
