0b8e6e1993b50513a710c9c54528fe32e1e61bed
Yusee <5376316+yusee@user.noreply.gitee.com>
2019-11-19 10:59:04 +0800

    修改

--- src/views/photo_sharing/PhotoSharingList.vue
+++ src/views/photo_sharing/PhotoSharingList.vue
@@ -5,11 +5,7 @@
       <van-tab v-for="item in message" :title="item.title" :key="item.id">
         <!-- 图片列表 -->
         <ul class="img_list">
-          <li
-            v-for="item in picList"
-            :key="item.id"
-            @click="goDetails(item.id)"
-          >
+          <li v-for="item in picList" :key="item.id" @click="goDetails(item.id)">
             <img class="van-image__img" v-lazy="item.img_url" />
             <div class="botInfo">
               <p>{{ item.title }}</p>
@@ -42,9 +38,7 @@ export default {
   methods: {
     // 获取图片列表页面上面的分类信息数据
     async getPhotoList() {
-      const { data: res } = await this.$http.get(
-        `http://www.liulongbin.top:3005/api/getimgcategory`
-      )
+      const { data: res } = await this.$http.get(`http://www.liulongbin.top:3005/api/getimgcategory`)
       // console.log(res)
       // 向 message 数组中，使用 unshift 方法手动添加"全部"对象
       res.message.unshift({ title: '全部', id: 0 })
@@ -53,18 +47,14 @@ export default {
 
     // 点击首页图片分享进入列表后直接获取“全部”选项所有数据
     async getAllList() {
-      const { data: res } = await this.$http.get(
-        'http://www.liulongbin.top:3005/api/getimages/0'
-      )
+      const { data: res } = await this.$http.get('http://www.liulongbin.top:3005/api/getimages/0')
       this.picList = res.message
     },
 
     // 获取点击首页上的“图片分享”后进入到的列表数据
     async toggleTab(index) {
       this.cateId = this.message[index].id
-      const { data: res } = await this.$http.get(
-        'http://www.liulongbin.top:3005/api/getimages/' + this.cateId
-      )
+      const { data: res } = await this.$http.get('http://www.liulongbin.top:3005/api/getimages/' + this.cateId)
       this.picList = res.message
       // console.log(this.picList)
     },
@@ -72,13 +62,13 @@ export default {
     // 点击图片跳转到对应的图片详情
     goDetails(id) {
       // 通过编程式导航跳转到对应图片id的图片详情
-      this.$router.push('/api/getthumimages/' + id)
+      this.$router.push('/photosharing/details/' + id)
     }
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 .img_list {
   padding: 0 10px;
 }
@@ -116,6 +106,6 @@ export default {
 }
 
 img {
-  object-fit: cover!important;
+  object-fit: cover !important;
 }
 </style>
